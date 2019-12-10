673782f3f7f31d1c51037ac3e021b7d6244c83f3
cavinleexing <252049240@qq.com>
2019-12-10 18:08:49 +0800

    commit
    Signed-off-by: cavinleexing <252049240@qq.com>

--- WEB/index.php
+++ WEB/index.php
@@ -2,12 +2,12 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>无标题文档</title>
+<title>PerfectWorld</title>
 </head>
 
 <body>
 <h1>TWorld Technology!&nbsp; &nbsp; Perfect world!</h1>
-<img src="../Users/Administrator/Desktop/TWorldTechLogo.png" width="500" height="500" alt=""/>
+<img src="./TWorldTechLogo.png" width="100" height="100" alt=""/>
 <div> T&nbsp; means Top and Team and Technology and Thunder!</div>
 </body>
 </html>
