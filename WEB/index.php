1f861cd62cd037eb0e274acfaa42b4868203539e
KingLee <252049240@qq.com>
2019-12-10 17:01:59 +0800

    client commit
    Signed-off-by: KingLee <252049240@qq.com>

--- WEB/index.php
+++ WEB/index.php
@@ -1,8 +1,13 @@
+<!doctype html>
 <html>
- <head>
-  <title>PHP 测试</title>
- </head>
- <body>
- <?php echo '<p>Hello Perfect World</p>'; ?>
- </body>
+<head>
+<meta charset="utf-8">
+<title>无标题文档</title>
+</head>
+
+<body>
+<h1>TWorld Technology!&nbsp; &nbsp; Perfect world!</h1>
+<img src="../Users/Administrator/Desktop/TWorldTechLogo.png" width="500" height="500" alt=""/>
+<div> T&nbsp; means Top and Team and Technology and Thunder!</div>
+</body>
 </html>
